a4efb1c33e051cbca96eff3899775ebb95f00f9b
rahim-xelpmoc <rahim.khan@xelpmoc.in>
2025-03-22 17:35:36 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='surya',
+    version='0.1.0',  # Replace with your desired version
+    packages=find_packages(),
+    install_requires=[
+        'pydantic'
+    ]
+)
